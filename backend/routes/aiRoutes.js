d93833af456502cd0fae37c9d8d6a9c1014e614f
sahilka5hyap <sahilksh2804@gmail.com>
2026-07-09 13:57:07 +0530

    Remove old backend, keep fitness-backend as the single backend

--- backend/routes/aiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { protect } = require("../middleware/authMiddleware");
-const { chatAI } = require("../controllers/aiController");
-
-router.post("/chat", protect, chatAI);
-
-module.exports = router;
